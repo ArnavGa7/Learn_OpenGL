730fcc5bc65313fc4a8bf5ecaabfda4335f6fb58
Arnav27893124623478 <12arnavgarg@gmail.com>
2026-08-13 22:22:11 +0200

    Added katana to the model Folder

--- Main.cpp
+++ Main.cpp
@@ -34,7 +34,7 @@ int main(){
 	glEnable(GL_DEPTH_TEST);
 
 	// Create the Object + the Light Sorce
-	Model Sword("Model/Sword/Sting-Sword-lowpoly.obj");
+	Model Sword("Model/Katana/model.obj");
 	Object light_sorce1(CubeVertices, CubeVerticesSize, CubeIndiecs, CubeIndiecsSize, "light.vert", "light.frag");
 
 
